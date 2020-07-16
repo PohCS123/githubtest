5bbe58d6c370cae70ad1fe1eb3406df1586051e3
Pohcs123 <gencapoh@gmail.com>
2020-07-16 10:31:51 +0800

    2nd commit

--- src/githubapplication/GithubApplication.java
+++ src/githubapplication/GithubApplication.java
@@ -15,7 +15,8 @@ public class GithubApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Ladies and Gentlemen");
+        System.out.println("Let me show a magic trick");
     }
     
 }
